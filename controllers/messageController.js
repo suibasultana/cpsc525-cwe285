64fa7ecdc3878d1ea141a53af0900c15bc28880b
Soila Pertet <nicolepertet@gmail.com>
2025-12-10 11:15:55 -0700

    Add functionality to handle unauthorised deletion of a message

--- controllers/messageController.js
+++ controllers/messageController.js
@@ -41,11 +41,22 @@ exports.handleAction = (req, res) => {
     
     const { action, id, content } = req.body;
 
+    // Find the message object corresponding to the provided id
+    const message = db.messages.find(message => message.id == parseInt(id));
+
+    // Return ERROR 404: NOT FOUND if message object cannot be found
+    if(!message) {
+        res.status(404).json({
+            error : `Couldn't find message with messageID ${id}.`
+        })
+    }
+
+    // Handle different use cases once message object is found
     switch(action) {
         case "edit":
-            return editMessage(id, content, res);
+            return editMessage(message, content, res);
         case "delete":
-            // Logic for handling unauthorised deletion of a message
+            return deleteMessage(id, res);
         case "flag":
             // Logic for handling unauthorised flaggin of a message
         case "forward":
@@ -55,20 +66,31 @@ exports.handleAction = (req, res) => {
     };
 };
 
-function editMessage(id, content, res) {
+// Handles Edit action
+function editMessage(message, content, res) {
     
-    // Find the message object to be edited
-    const message = db.messages.find(message => message.id == id);
-
     // Update the message content to the given content
     message.content = content;
 
     // Save updated message object
-    const updatedMessage = message;
+    const editedMessage = message;
 
     // Return response showing message was edited
     res.json({
-        message : "Successfully altered message.",
-        data : updatedMessage
+        message : "VULNERABLE ACTION: UNAUTHORISED EDITING",
+        data : editedMessage
     });
+}
+
+// Handles Delete action
+function deleteMessage(id, res) {
+    
+    // Delete message object from messages array with corresponding id
+    const editedMessages = db.messages.filter(m => m.id !== parseInt(id));
+
+    // Return response showing message was deleted
+    res.json({
+        message : "VULNERABLE ACTION: UNAUTHORISED DELETION",
+        data : editedMessages
+    })
 }
